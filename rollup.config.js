7a16bd1b7582b430061c714148c7fdebe8456ca9
chandwaniac <149376785+chandwaniac@users.noreply.github.com>
2025-12-19 18:13:49 +0530

    Update rollup config to use @rollup/plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default {
